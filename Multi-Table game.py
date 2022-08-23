7b0dde5f9cd9fe26fef1a90d99454838f4f2da60
Ezekiel Penrod <104532098+EJpenrod@users.noreply.github.com>
2022-08-22 20:38:37 -0700

    Update Multi-Table game.py

--- Multi-Table game.py
+++ Multi-Table game.py
@@ -3,30 +3,37 @@ import random
 
 def times_table_guess():
     score = 0
+    valid = False
     while True:
         x = random.randint(1, 10)
         y = random.randint(1, 10)
         solution = x * y
         print("what is {} * {}?\n".format(x, y))
-        guess = int(input("What is the solution?: "))
+        while not valid:  # loop until the user enters a valid int
+            try:
+                guess = int(input("enter the answer: "))
+                valid = True  # if this point is reached, x is a valid int
+            except ValueError:
+                print('Please only type in a number')
         if guess == solution:
             print("""
-____________________
-    correct
-____________________""")
+.___________________.
+|      correct      |
+|___________________|""")
             score += 1
         else:
             print("""
-____________________
-    incorrect
-____________________, the answer is {}\n""".format(solution))
-        print("your score is {}\n".format(score))
+.___________________.
+|     incorrect     |
+|___________________|, the answer is {}\n""".format(solution))
+        print("your score is [{}]\n********************\n".format(score))
+        valid = False
         if score == 20:
             print("""
 ____ ____ _  _ ____ ____ ____ ___ _  _ _    ____ ___ _ ____ _  _ ____ |
 |    |  | |\ | | __ |__/ |__|  |  |  | |    |__|  |  | |  | |\ | [__  |
 |___ |__| | \| |__] |  \ |  |  |  |__| |___ |  |  |  | |__| | \| ___] .
-_   _ ____ _  _ . ____ ____    ____ _    _       ___  ____ _  _ ____
+_   _ ____ _  _   ____ ____    ____ _    _       ___  ____ _  _ ____
  \_/  |  | |  | ' |__/ |___    |__| |    |       |  \ |  | |\ | |___
   |   |__| |__|   |  \ |___    |  | |___ |___    |__/ |__| | \| |___
 """)
